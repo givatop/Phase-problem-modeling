c5ed09a42aba9aca9dac0173a024c5f3950d47f0
megamott <megamott27@gmail.com>
2021-06-01 18:16:27 +0300

    Поменял все __ на _ в классе Grid и его дочерних классах (приватные методы по PEP8).

--- src/propagation/model/areas/grid.py
+++ src/propagation/model/areas/grid.py
@@ -8,36 +8,36 @@ from ...utils.math.units import px2m
 
 class Grid(ABC):
     def __init__(self, height, width, pixel_size):
-        self.__height = height
-        self.__width = width
-        self.__pixel_size = pixel_size
+        self._height = height
+        self._width = width
+        self._pixel_size = pixel_size
 
     @property
     def pixel_size(self) -> float:
         """ Размер пикселя матрицы [м] """
-        return self.__pixel_size
+        return self._pixel_size
 
     @pixel_size.setter
     def pixel_size(self, pixel_size):
-        self.__pixel_size = pixel_size
+        self._pixel_size = pixel_size
 
     @property
     def height(self) -> float:
         """ Высота матрицы [px] """
-        return self.__height
+        return self._height
 
     @height.setter
     def height(self, height):
-        self.__height = height
+        self._height = height
 
     @property
     def width(self) -> float:
         """ Ширина матрицы [px] """
-        return self.__width
+        return self._width
 
     @width.setter
     def width(self, width):
-        self.__width = width
+        self._width = width
 
 
 class CoordinateGrid(Grid):
@@ -45,14 +45,14 @@ class CoordinateGrid(Grid):
 
     def __init__(self, height, width, pixel_size=5.04e-6):
         super().__init__(height, width, pixel_size)
-        self.__grid = px2m(np.mgrid[
+        self._grid = px2m(np.mgrid[
                            -self.height / 2:self.height / 2,
                            -self.width / 2:self.width / 2
-                           ])
+                          ])
 
     @property
     def grid(self) -> Tuple[np.ndarray, np.ndarray]:
-        return self.__grid
+        return self._grid
 
 
 class PolarGrid(Grid):
@@ -61,8 +61,8 @@ class PolarGrid(Grid):
     def __init__(self, coordinate_grid: CoordinateGrid):
         """ Создаёт сетку  в полярных координатах на основе сетки в квадратичных координатах """
         super().__init__(coordinate_grid.height, coordinate_grid.width, coordinate_grid.pixel_size)
-        self.__polar_grid = np.sqrt(sum(map(lambda x: x * x, coordinate_grid.grid)))
+        self._polar_grid = np.sqrt(sum(map(lambda x: x * x, coordinate_grid.grid)))
 
     @property
     def polar_grid(self) -> Tuple[np.ndarray, np.ndarray]:
-        return self.__polar_grid
+        return self._polar_grid
