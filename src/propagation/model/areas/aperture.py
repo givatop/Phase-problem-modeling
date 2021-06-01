ddfc45ae35b5a9ed50bc4dd157022969112b827e
megamott <megamott27@gmail.com>
2021-06-01 18:19:38 +0300

    Рефакторинг документации класса Aperture.

--- src/propagation/model/areas/aperture.py
+++ src/propagation/model/areas/aperture.py
@@ -2,36 +2,36 @@
 from src.propagation.utils.optic.field import circ
 from .grid import PolarGrid
 
-from ...utils.math import units
+from ...utils.math.units import px2m
 
 
 class Aperture:
     """ Апертура, circ """
 
-    def __init__(self, radial_area: PolarGrid, aperture_diameter: float):
+    def __init__(self, polar_grid: PolarGrid, aperture_diameter: float):
         """
-        Создаёт круглую апертуру на основе сетки сферических координат
-        :param radial_area: сетки сферических координат
+        Создаёт апертуру (circ) на основе сетки в полярных координатах
+        :param polar_grid: сетка в полярных координатах
         :param aperture_diameter: диаметр апертуры [px]
         """
-        aperture_diameter = units.px2m(aperture_diameter, px_size_m=radial_area.pixel_size)  # [м]
-        self.__aperture_diameter = aperture_diameter
-        self.__radial_area = radial_area
-        self.__aperture = circ(radial_area.polar_grid, w=aperture_diameter)
+        aperture_diameter = px2m(aperture_diameter, px_size_m=polar_grid.pixel_size)  # [м]
+        self._aperture_diameter = aperture_diameter
+        self._radial_area = polar_grid
+        self._aperture = circ(polar_grid.polar_grid, w=aperture_diameter)
 
     @property
     def aperture_diameter(self):
-        return self.__aperture_diameter
+        return self._aperture_diameter
 
     @aperture_diameter.setter
     def aperture_diameter(self, aperture_diameter):
-        self.__aperture_diameter = units.px2m(aperture_diameter, px_size_m=self.__radial_area.pixel_size)  # [м]
-        self.__aperture = circ(self.radial_area.polar_grid, w=self.__aperture_diameter)
+        self._aperture_diameter = px2m(aperture_diameter, px_size_m=self._radial_area.pixel_size)  # [м]
+        self._aperture = circ(self.radial_area.polar_grid, w=self._aperture_diameter)
 
     @property
     def radial_area(self):
-        return self.__radial_area
+        return self._radial_area
 
     @property
     def aperture(self):
-        return self.__aperture
+        return self._aperture
