5bda9c67d0a99864fa288fd0a223153c1b280db1
Iliya Gritsenko <givatop@gmail.com>
2021-08-05 23:53:50 +0300

    Исследование 1D Градиента и Лапласиана

--- src/propagation/utils/math/derivative/fourier.py
+++ src/propagation/utils/math/derivative/fourier.py
@@ -1,4 +1,4 @@
-from numpy.fft import fft2, ifft2
+from numpy.fft import fft2, ifft2, fft, ifft
 from numpy import ndarray, real
 """
 Псевдо-дифференциальные операторы, реализованные через FFT.
@@ -51,6 +51,32 @@ def ilaplacian_2d(f: ndarray,
     return res
 
 
+def ilaplacian_1d(f: ndarray,
+                  kx: ndarray,
+                  return_spacedomain: bool = True) -> ndarray:
+    """
+    Возвращает сумму частных производных минус второго порядка (обратный Лапласиан) от функции f.
+    :param f: array-like двумерная функция
+    :param kx: частотный коэффициент 1j * 2*np.pi * fftshift(nu_x_grid)
+    :param return_spacedomain:
+    :return: array-like градиент от функции f
+    """
+    # Create mask
+    mask = (kx == 0)
+    kx[mask] = 1. + 0*1j
+    # Spectral Transformation
+    res = fft(f, norm=NORM) / kx.T ** 2
+    # Correct result array
+    res[mask] = 0. + 0*1j
+    # Correct kx
+    kx[mask] = 0. + 0*1j
+
+    if return_spacedomain:
+        res = ifft(res, norm=NORM).real
+
+    return res
+
+
 if __name__ == '__main__':
     import numpy as np
     import matplotlib.pyplot as plt
@@ -66,15 +92,18 @@ if __name__ == '__main__':
 
     # Параметры y(x)
     a = 1
-    period = width / 40  # 2 * np.pi
-    x0 = -period
+    period = width / 2  # 2 * np.pi
+    # x0 = 0
+    x0 = -width/8
     left = 0  # np.pi * 3
     right = period*3.5  # period * 4.5
     clip = 0
 
     # y(x)
+    # y = triangle_1d(x, x0=x0, w=period)
     y = logistic_1d(x, x0=x0, w=period) * logistic_1d(-x, x0=x0, w=period)
-    y = np.roll(y, 512)
+    # y[0] = y[-1]
+    # y = np.roll(y, 511)
     # y = sin_1d(x, x0=x0, T=period, right=right, left=left, clip=clip)
 
     # y'(x)
@@ -91,37 +120,106 @@ if __name__ == '__main__':
     # y_grad_difference_fft = abs(y_grad_analitic - y_grad_fft)
 
     # Error
-    y_error = abs(y_grad_np - y_grad_fft)
+    y_grad_error = abs(y_grad_np - y_grad_fft)
 
     print(f'N = {num: >5} \t\t '
-          f'w = {width: >10} \t\t ')
-          # f'Numpy Error: {np.max(y_grad_difference_np): >15} \t\t '
-          # f'FFT Error: {np.max(y_grad_difference_fft): >15}')
+          f'w = {width: >10} \t\t '
+          f'error = {max(y_grad_error)}')
 
-    fig = plt.figure(figsize=[8.4, 6.8])  # 6.4, 4.8
+    # Gradient Plots
+    # fig = plt.figure(figsize=[7.4, 5.8])  # 6.4, 4.8
+    # ax1, ax2 = fig.add_subplot(2, 1, 1), fig.add_subplot(2, 1, 2)
+    #
+    # :threshold = len(x)
+    # ax1.plot(x[::threshold], y[::threshold], label='f1 = sin(x)')
+    # # ax1.plot(x[::threshold], y_grad_analitic[::threshold], label='f2 = cos(x)', linestyle='--')
+    # ax1.plot(x[::threshold], y_grad_np[::threshold], label=f'y\'(x) - numpy', linestyle='dotted')
+    # ax1.plot(x[::threshold], y_grad_fft[::threshold], label=f'y\'(x) - fft', linestyle='dotted')
+    #
+    # # ax2.plot(x, y_grad_difference_np, label='abs(f2 - f3)', linestyle='dotted')
+    # # ax2.plot(x, y_grad_difference_fft, label='abs(f2 - f4)', linestyle='--')
+    # ax2.plot(x, y_grad_error, label='|numpy - fft|', linestyle='--')
+    #
+    # ax1.grid()
+    # ax1.legend(prop={'size': 12}, bbox_to_anchor=(1.05, 1.0))  #, bbox_to_anchor=(.5, 1.7)
+    # ax1.set_title(f'Grads. Width = {width:.5f}. Num = {num}. T = {period:.5f}')
+    #
+    # ax2.grid()
+    # ax2.legend(prop={'size': 12}, bbox_to_anchor=(1.05, 1.0))  #, bbox_to_anchor=(1., 1.7)
+    # ax2.set_yscale('log')
+    # ax2.set_title('FFT error')
+    #
+    # fig.suptitle('Gradient')
+    # fig.tight_layout()
+
+    # Numpy Laplacian
+    y_laplacian_np = np.gradient(y_grad_np, dx)
+
+    # f(x) <-- Gradient
+    mask = (kx == 0)
+    kx[mask] = 1.+0*1j
+    y_fft = fft(y_grad_fft) / kx
+    y_fft[mask] = 0.+0*1j
+    y_igrad_fft = ifft(y_fft).real
+    kx[mask] = 0.+0*1j
+
+    y_igrad_fft += y[0] - y_igrad_fft[0]
+
+    # FFT Laplacian
+    y_laplacian_fft = ifft(kx**2 * fft(y)).real
+
+    # FFT Laplacian --> Gradient
+    kx[mask] = 1.+0*1j
+    y_grad_from_laplacian_fft = fft(y_laplacian_fft) / kx
+    y_grad_from_laplacian_fft[mask] = 0. + 0 * 1j
+    y_grad_from_laplacian_fft = ifft(y_grad_from_laplacian_fft).real
+    kx[mask] = 0.+0*1j
+
+    # FFT f(x) <-- Laplacian
+    kx[mask] = 1.+0*1j
+    y_from_laplacian_fft = fft(y_laplacian_fft) / kx**2
+    y_from_laplacian_fft[mask] = 0. + 0 * 1j
+    y_from_laplacian_fft = ifft(y_from_laplacian_fft).real
+    kx[mask] = 0.+0*1j
+
+    # Errors
+    y_laplacian_error = np.abs(y_laplacian_np - y_laplacian_fft)
+    y_error_f_from_grad = np.abs(y - y_igrad_fft)
+    y_error_grad_from_laplacian_fft = np.abs(y_grad_fft - y_grad_from_laplacian_fft)
+    y_error_from_laplacian_fft = np.abs(y - y_from_laplacian_fft)
+
+    # Laplacian Plots
+    fig = plt.figure(figsize=[7.4, 6.8])  # 6.4, 4.8
     ax1, ax2 = fig.add_subplot(2, 1, 1), fig.add_subplot(2, 1, 2)
 
-    threshold = len(x)
-    ax1.plot(x[:threshold], y[:threshold], label='f1 = sin(x)')
-    # ax1.plot(x[:threshold], y_grad_analitic[:threshold], label='f2 = cos(x)', linestyle='--')
-    ax1.plot(x[:threshold], y_grad_np[:threshold], label='f3 = np.gradient(sin(x))', linestyle='dotted')
-    ax1.plot(x[:threshold], y_grad_fft[:threshold], label='f4 = ifft(kx * fft(sin(x)))', linestyle='dotted')
+    threshold = 8
+    ax1.plot(x[::threshold], y[::threshold], 'b-', label='y(x)')
+    ax1.plot(x[::threshold], y_grad_fft[::threshold], 'g-', label='y\'(x) - fft')
+    ax1.plot(x[::threshold], y_laplacian_fft[::threshold], 'r-', label='y\'\'(x) - fft')  # # todo уехал вверх...
+    # ax1.plot(x[::threshold], y_laplacian_np[::threshold], 'y-', label='y\'\'(x) - np')
+    ax1.plot(x[::threshold], y_igrad_fft[::threshold], 'c*', label='y(x) - from gradient')
+    ax1.plot(x[::threshold], y_grad_from_laplacian_fft[::threshold], 'm:', label='y\'(x) - from laplacian')
+    ax1.plot(x[::threshold], y_from_laplacian_fft[::threshold], 'k+', label='y(x) - from laplacian')
 
-    # ax2.plot(x, y_grad_difference_np, label='abs(f2 - f3)', linestyle='dotted')
-    # ax2.plot(x, y_grad_difference_fft, label='abs(f2 - f4)', linestyle='--')
-    ax2.plot(x, y_error, label='|numpy - fft|', linestyle='--')
+    ax2.plot(x[::threshold], y_grad_error[::threshold], 'g-', label=f'gradient: {max(y_grad_error):.1e}')
+    ax2.plot(x[::threshold], y_laplacian_error[::threshold], 'r-', label=f'laplacian: {max(y_laplacian_error):.1e}')
+    ax2.plot(x[::threshold], y_error_f_from_grad[::threshold], 'c*', label=f'y(x) from gradient: {max(y_error_f_from_grad):.1e}')
+    ax2.plot(x[::threshold], y_error_grad_from_laplacian_fft[::threshold], 'm:', label=f'grad from laplace: {max(y_error_grad_from_laplacian_fft):.1e}')
+    ax2.plot(x[::threshold], y_error_from_laplacian_fft[::threshold], 'k+', label=f'y(x) from laplace: {max(y_error_from_laplacian_fft):.1e}')
 
     ax1.grid()
-    ax1.legend(prop={'size': 12}, bbox_to_anchor=(1.05, 1.0))  #, bbox_to_anchor=(.5, 1.7)
-    ax1.set_title(f'Grads. Width = {width:.5f}. Num = {num}. T = {period:.5f}')
+    ax1.legend(prop={'size': 12}, bbox_to_anchor=(1.05, 1.0))
+    ax1.set_title(f'Width = {width:.5f}. Num = {num}. T = {period:.5f}')
 
     ax2.grid()
-    ax2.legend(prop={'size': 12}, bbox_to_anchor=(1.05, 1.0))  #, bbox_to_anchor=(1., 1.7)
+    ax2.legend(prop={'size': 12}, bbox_to_anchor=(1.05, 1.0))
     ax2.set_yscale('log')
-    ax2.set_title('FFT error')
+    ax2.set_title('Error = |numpy - fft|')
 
+    fig.suptitle('Laplacian')
     fig.tight_layout()
 
+    # Spectral Plots
     # fig2 = plt.figure(figsize=[8.4, 7.8])  #  6.4, 4.8
     # ax3, ax4, ax5, ax6, ax7 = \
     #     fig2.add_subplot(5, 1, 1), \
