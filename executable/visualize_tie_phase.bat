040dd06a2e37f3ea25347573a227e2768ad94e51
Iliya Gritsenko <givatop@gmail.com>
2021-09-17 13:08:24 +0300

    Новый вариант перебора файлов (но его тоже хватает только на 42 аргумента)

--- executable/visualize_tie_phase.bat
+++ executable/visualize_tie_phase.bat
@@ -1,7 +1,9 @@
-echo path to python
+@echo off
+
+rem path to python
 set python="C:\Users\IGritsenko\.virtualenvs\Phase-problem-modeling-VD4lAbA6\Scripts\python.exe"
 
-echo path to visualizer.py
+rem path to visualizer.py
 set visualizer="C:\Users\IGritsenko\Documents\Python Scripts\TIE v2\Phase-problem-modeling\executable\visualizer.py"
 
 rem choices=['complex_amplitude', 'slice', 'error', 'tie_phase'],
@@ -16,21 +18,12 @@ set phase_ylabel="rad"
 set show_plot=0
 set save_plot=1
 
-
-rem обход по всем файлам
-for	%%f in (%*) do call :loop %%f
-
-
 :loop
-rem путь к файлу
-set file_path=%1
-rem папка куда сохраняем
-set save_folder="%~dp1\"
+if "%~1" neq "" (
+  echo %~1
+  %python% %visualizer% --file_path "%~1" --save_folder "%~dp1\" --mode %mode% --dpi %dpi% --figsize %figsize_width% %figsize_height% --px_size %px_size% --cmap %cmap% --show_plot %show_plot% --save_plot %save_plot% --phase_ylabel %phase_ylabel%
 
-
-echo running...
-%python% %visualizer% --file_path %file_path% --save_folder %save_folder% --mode %mode% --dpi %dpi% --figsize %figsize_width% %figsize_height% --px_size %px_size% --cmap %cmap% --show_plot %show_plot% --save_plot %save_plot% --phase_ylabel %phase_ylabel%
-
-rem pause
-
-exit /b
+  shift
+  goto :loop
+)
+pause
