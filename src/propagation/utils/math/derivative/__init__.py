25e24ce44465e198240176b817b49939af26c3e5
Iliya Gritsenko <givatop@gmail.com>
2021-10-06 15:16:19 +0300

    Добавлен init.py для директории derivative

---
+++ src/propagation/utils/math/derivative/__init__.py
@@ -0,0 +1,11 @@
+from .finite_difference import central_finite_difference
+from .fourier import (
+    gradient_1d,
+    gradient_2d,
+    ilaplacian_1d,
+    ilaplacian_2d,
+    dct_gradient_1d,
+    dct_gradient_2d,
+    dct_ilaplacian_1d,
+    dct_ilaplacian_2d,
+)
