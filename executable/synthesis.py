2ab2818d16725418f4588e8ec59bbe3cc6456f61
Iliya Gritsenko <givatop@gmail.com>
2021-10-07 12:51:04 +0300

    Рефакторинг synthesis.py

--- executable/synthesis.py
+++ executable/synthesis.py
@@ -10,7 +10,6 @@ from datetime import date
 import numpy as np
 
 from src.propagation.presenter.loader import load_image
-from src.propagation.utils.math.units import (nm2m, mm2m, um2m, m2mm, px2m, m2nm, m2um)
 from src.propagation.utils.math.general import calculate_chord
 import src.propagation.utils.optic as optic
 
@@ -27,43 +26,40 @@ folder = '/Users/megamot/Programming/Python/Phase-problem-modeling/data/executab
 
 filename = 'i=0.5 phi=sphere 1D complex_field.npy'
 
-# region Grid Params
+# Grid Params
 width, height = 512, 512
 shape = [width] if height == 1 else [width, height]
-# px_size = um2m(5)
-# wavelength = nm2m(555)
 x = np.arange(-width // 2, width // 2)
 y = np.arange(-height // 2, height // 2)
 X, Y = np.meshgrid(x, y)
-# Y, X = np.mgrid[
-#         -height // 2: height // 2,
-#         -width // 2: width // 2,
-#     ]
-# endregion
 
-# region Intensity
+# Intensity
 if IS_INTENSITY_FROM_IMAGE:
     intensity = load_image(i_path)
 else:
     i_amplitude = 0.5
     i_wx, i_wy = width / 10, height / 2
     i_x0, i_y0 = 0, 0
-    intensity = i_amplitude
+    intensity = i_amplitude * np.ones(shape)
     # intensity = gauss_1d(x, a=i_amplitude, w=i_wx, x0=i_x0)
-# endregion
-# region Phase
+
+# Phase
 if IS_PHASE_FROM_IMAGE:
     phase = load_image(p_path)
 else:
-    p_amplitude = 1.
+    p_amplitude = 1
     p_wx, p_wy = width / 4, height / 2
-    p_x0, p_y0 = p_wx // 4, 0
-    focus = mm2m(100)
-    phase = np.sqrt(px2m(x, px_size_m=px_size) ** 2 + focus ** 2) * 2 * np.pi / wavelength
-    # phase = sin_1d(x, a=p_amplitude, T=p_wx, x0=p_x0)
-# endregion
+    p_x0, p_y0 = 0, 0
+    radius = 350_000
+    sag = 0.01
+    chord = calculate_chord(radius, sag)
+    phase = \
+        optic.hemisphere(X, Y, sag=sag, r=radius, x0=-chord * 0.5, y0=-chord * 0.75) + \
+        optic.hemisphere(X, Y, sag=sag, r=radius, x0=-chord * 0.5, y0= chord * 0.75) + \
+        optic.hemisphere(X, Y, sag=sag, r=radius, x0= width * 0.5 - chord * 0.5, y0=-chord * 0.75) + \
+        optic.hemisphere(X, Y, sag=sag, r=radius, x0= width * 0.5 - chord * 0.5, y0= chord * 0.75)
 
-# noise
+# Noise
 if ADD_NOISE:
     # params
     mean = 0.0
@@ -78,22 +74,22 @@ if ADD_NOISE:
 
 # Aperture
 if ADD_APERTURE:
-    a_wx, a_wy = width // 2, height //2
+    a_wx, a_wy = width // 2, height // 2
     a_x0, a_y0 = 0, 0
     if intensity.ndim == 1:
-        aperture = rect_1d(x, a=1, w=a_wx, x0=a_x0)
+        aperture = optic.rect_1d(x, a=1, w=a_wx, x0=a_x0)
     elif intensity.ndim == 2:
-        aperture = rect_2d(x, y, a=1, wx=a_wx, wy=a_wy, x0=a_x0, y0=a_y0)
+        aperture = optic.rect_2d(x, y, a=1, wx=a_wx, wy=a_wy, x0=a_x0, y0=a_y0)
 else:
     aperture = np.ones(shape)
 
-# region Complex Field
+# Complex Field
 complex_field = np.sqrt(intensity) * np.exp(1j * phase) * aperture
-# endregion
-# region Save Complex Field
+
+# Save
 filepath = os.path.join(folder, filename)
 np.save(filepath, complex_field)
-# endregion
+
 # region Metadata
 filename += '.metadata'
 filepath = os.path.join(folder, filename)
@@ -101,8 +97,6 @@ filepath = os.path.join(folder, filename)
 metadata['date'] = date.today().strftime("%d/%m/%Y")
 metadata['width, px'] = width
 metadata['height, px'] = height
-metadata['wavelength, nm'] = m2nm(wavelength)
-metadata['px_size, um'] = m2um(px_size)
 
 if IS_INTENSITY_FROM_IMAGE:
     metadata['i_path'] = i_path
@@ -121,6 +115,9 @@ else:
     metadata['p_wy, px'] = p_wy
     metadata['p_x0, px'] = p_x0
     metadata['p_y0, px'] = p_y0
+    metadata['radius, px'] = radius
+    metadata['sag, px'] = sag
+    metadata['chord, px'] = chord
 
 with open(filepath, 'w') as file:
     for k, v in metadata.items():
