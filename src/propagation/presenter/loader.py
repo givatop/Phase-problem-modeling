2b340705138e6bad3b156bc04f7a260e266439b6
Iliya Gritsenko <givatop@gmail.com>
2021-11-11 23:19:43 +0300

    Доработан метод load_txt для загрузки файлов из ПО Fresnel. Можно загружать файлы с частично-неопределенной фазой

--- src/propagation/presenter/loader.py
+++ src/propagation/presenter/loader.py
@@ -34,15 +34,16 @@ def load_txt(path: str) -> np.ndarray:
     """
     try:
         array = np.loadtxt(path)
-        # from Fresnel
-        if array.ndim == 2 and array.shape[1] == 2:  # 2 columns (x, y) instead of 1 (y)
-            array = array[::, 1]
     except ValueError:
         with open(path) as f:
             lines = f.readlines()
-        lines = [line.strip().split('\t')[1:] for line in lines]  # delete y values col
-        lines.pop(0)  # delete x values row
+
+        lines.pop(0)  # cut first row without data
+        lines = [line.strip().split('\t')[1:] for line in lines]  # save Y-values col without X-values col
+        [line.append('0.0') for line in lines if len(line) == 0]  # Fresnel write nothing instead of zero in the phase txt file
         array = np.asarray(lines, dtype=float)
+        if array.shape[1] == 1:
+            array = array.reshape([array.shape[0]])  # "transpose" col-vector to row-vector
 
     return array
 
@@ -72,3 +73,9 @@ def load_image(path: str) -> np.ndarray:
         old_max = 2 ** 8 - 1
 
     return normalize(np.asarray(img, np.float64), old_min=0, old_max=old_max)
+
+
+if __name__ == '__main__':
+    path = r'\\hololab.ru\store\Рабочие папки K-Team\Гриценко\1. Работа\1. Проекты\2021 РНФ TIE\1. Данные\1. Тестовые\11. GAUSS\Fresnel\Новая папка\phi z=0.000.txt'
+    f = load_file(path)
+    print(abs(f.min()) + abs(f.max()))
