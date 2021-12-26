8ac5f2348022b0d271fb0396a0015b4b04141b90
Iliya Gritsenko <givatop@gmail.com>
2021-12-26 16:48:08 +0300

    Рефакторинг: убраны багованные импорты

--- src/propagation/__init__.py
+++ src/propagation/__init__.py
@@ -1,9 +1,9 @@
-from .model.waves.spherical_wave import SphericalWave
-from .model.areas.grid import CartesianGrid
-from .model.areas.grid import PolarGrid
-from .model.areas.grid import FrequencyGrid
-from .model.areas.aperture import Aperture
-from .utils.math import units
-from .utils.optic import propagation_methods
+# from .model.waves.spherical_wave import SphericalWave
+# from .model.areas.grid import CartesianGrid
+# from .model.areas.grid import PolarGrid
+# from .model.areas.grid import FrequencyGrid
+# from .model.areas.aperture import Aperture
+# from .utils.math import units
+# from .utils.optic import propagation_methods
 
 __version__ = "2.0.0.dev"
