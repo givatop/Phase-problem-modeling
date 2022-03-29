9651e3984fed1978cd12240abf8f5cdcca96ac84
Iliya Gritsenko <givatop@gmail.com>
2022-03-29 15:12:50 +0300

    Уточнение координат битых пикселей и рефакторинг

--- src/papers/broken_px_filter.py
+++ src/papers/broken_px_filter.py
@@ -5,21 +5,26 @@ from PIL import Image
 from src.propagation.presenter.loader import load_file
 from src.propagation.utils.math.general import normalize
 
-folder = r'\\hololab.ru\store\Рабочие папки K-Team\Сцепуро\М2\12.09'
+folder = r'\\hololab.ru\store\Рабочие папки K-Team\Гриценко\1. Работа' \
+         r'\1. Проекты\2022 Vortex\2. Эксперимент\2022 март 28'
 
-dzs = np.arange(0, 144)
+distances = np.arange(100, 101, 1)
 
-for dz in dzs:
-
-    print(f'dz: {dz:.3f} mm')
+for z in distances:
 
     # Загружаем из файла
-    filename = f'{dz:0>3.0f}.tif'
+    """
+    z=0.{z:0>3.0f}
+    z={z:.0f}.{"":0>3}
+    """
+    filename = f'z={z:.0f}.{"":0>3}.tif'  #
     path = os.path.join(folder, filename)
     array = load_file(path)
 
+    print(filename)
+
     # Вырезаем нужную область
-    array[490:540, 740:800] = array[490, 750]
+    array[496:536, 754:784] = array[495, 750]
 
     # Нормировка на 126 бит
     array = normalize(array, old_min=0, old_max=1.,
@@ -28,6 +33,6 @@ for dz in dzs:
     image = Image.fromarray(array, mode='I;16')
 
     # Сохранение в файл
-    filename = f'f {dz:.1f}.tif'
+    filename = f'f {filename}'
     path = os.path.join(folder, filename)
     image.save(path)
